588283fb3d159a2fcb96a7db5a2076095c9f4b6e
Luis Eduardo <vsluiseduardo01@gmail.com>
2024-07-31 19:53:59 -0400

    LazyColumn com lista de filmes.

--- app/src/main/java/br/com/luise/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/luise/movieapp/MainActivity.kt
@@ -1,16 +1,51 @@
 package br.com.luise.movieapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.luise.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +53,79 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp { innerPadding ->
+                MainContent(contentPadding = innerPadding)
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable (PaddingValues) -> Unit) {
+    MovieAppTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Simple Scaffold Screen") },
+                    colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Magenta)
+                )
+            },
+            content = { innerPadding ->
+                content(innerPadding)
+            }
+        )
+    }
 }
+
+@Composable
+fun MainContent(
+    contentPadding: PaddingValues,
+    movieList: List<String> = listOf("Avatar", "300", "Harry Potter", "LASDAS", "ASDAS", "AAAA", "AAA")
+) {
+    LazyColumn(
+        modifier = Modifier.consumeWindowInsets(contentPadding),
+        contentPadding = contentPadding
+    ) {
+        items(movieList) { movie ->
+            MovieRow(movie = movie)
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(modifier = Modifier
+        .padding(10.dp)
+        .fillMaxWidth()
+        .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp),
+        onClick = { /*TODO*/ }) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                tonalElevation = 4.dp,
+                shadowElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+    MyApp { innerPadding ->
+        MainContent(contentPadding = innerPadding)
     }
 }
